6e291c8f6ddbeed39f3552baba4c988c33618991
Reisuha <dashenka.kazanceva@gmail.com>
2023-11-07 18:54:11 +0300

    ifaus

---
+++ bin/__tests__/index.test.js
@@ -0,0 +1,64 @@
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+import genDiff from '../src/index.js';
+import formatStylish from '../src/formatters/stylish.js';
+import formatPlain from '../src/formatters/plain.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const fullReadFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+
+
+const extentions = ['json', 'yml'];
+const formats = ['stylish', 'plain'];
+const expStylish = fullReadFile('expectedStylishFormat.txt');
+const expJson = fullReadFile('expectedJsonFormat.txt');
+const expPlain = fullReadFile('expectedPlainFormat.txt');
+
+describe('Correct format testing', () => {
+  test.each(extentions)('testing %s', (extension) => {
+    const file1 = getFixturePath(`file3.${extension}`);
+    const file2 = getFixturePath(`file4.${extension}`);
+
+    expect(genDiff(file1, file2)).toEqual(expStylish);
+
+    expect(genDiff(file1, file2, 'stylish')).toEqual(expStylish);
+
+    expect(genDiff(file1, file2, 'json')).toEqual(expJson);
+    expect(genDiff(file1, file2, 'json')).toEqual(expJson);
+
+    expect(genDiff(file1, file2, 'json')).toEqual(expJson);
+
+    expect(genDiff(file1, file2, 'plain')).toEqual(expPlain);
+
+    expect(genDiff(file1, file2, 'plain')).toEqual(expPlain);
+  });
+});
+
+describe('Check the wrong format', () => {
+  test('testing', () => {
+    expect(() => genDiff('__fixtures__/file1.json', '__fixtures__/file2.json', 'wrongFormat'))
+      .toThrow('Unfortunately, this format is not supported.');
+  });
+});
+
+describe('Check the wrong extension', () => {
+  test.each(extentions)('testing', () => {
+    expect(() => genDiff(`__fixtures__/file1.${!extentions}`, `__fixtures__/file2.${!extentions}`, 'format'))
+      .toThrow('Invalid extension');
+  });
+});
+
+describe('Check for incorrect type of file change', () => {
+  test.each(formats)('testing %s', () => {
+    const data = [
+      { key: 'name', type: 'nested', children: [] },
+      { key: 'age', type: 'added', value: 12 },
+      { key: 'address', type: 'invalid', value: 'Saint-Peterburg' },
+    ];
+    expect(() => formatStylish(data)).toThrow('Unknown type: ');
+    expect(() => formatPlain(data)).toThrow('Unknown type: ');
+  });
+});
---
+++ bin/gendiff.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+import { program } from 'commander';
+import genDiff from '../src/index.js';
+
+program
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0', '-V, --version', 'output the version number')
+  .option('-f, --format <type>', 'output format', 'stylish')
+  .arguments('<filepath1> <filepath2>')
+  .helpOption('-h, --help', 'output usage information')
+  .action((filepath1, filepath2) => {
+    console.log(genDiff(filepath1, filepath2, program.opts().format));
+  });
+program.parse(process.argv);
